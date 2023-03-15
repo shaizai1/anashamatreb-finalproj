78f6e9bbff8208f6281772b4de8fc3770dca5174
Matthew <mbrooks9622@gmail.com>
2023-03-15 19:27:23 +0000

    search bar placeholder using vite from library

---
+++ MovieSearch/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import { SearchBar } from './components/SearchBar';
+
+function App() {
+
+
+  return <div className="App">Hello World!<div className="search-bar-container">
+    <SearchBar />
+    <div>SearchResults</div>
+    
+    </div></div>;
+  
+}
+
+export default App
---
+++ MovieSearch/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const SearchBar = () => {
+    return <div className="input-wrapper">
+    
+    <input placeholder="Type to search..."/>
+    </div>;
+
+}
