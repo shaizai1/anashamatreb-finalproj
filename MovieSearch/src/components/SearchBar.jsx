a7234244ce5afcc2bdfdcbf41b86ece60337b039
Matthew <mbrooks9622@gmail.com>
2023-03-15 19:44:04 +0000

    set the input variable to store the value in the placeholder usinge.target.value

--- MovieSearch/src/components/SearchBar.jsx
+++ MovieSearch/src/components/SearchBar.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import "./SearchBar.css";
 
 export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+
     return <div className="input-wrapper">
-    
-    <input placeholder="Type to search..."/>
+
+        <input
+            placeholder="Type to search..."
+
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+        />
     </div>;
 
 }
