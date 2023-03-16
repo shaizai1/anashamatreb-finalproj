a173ec6017fe59c7ee74ab1a96a3e8dc5c2e4fd7
shaizai1 <117948803+shaizai1@users.noreply.github.com>
2023-03-16 15:05:26 +0000

    basic outline for section1

---
+++ src/components/section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MoviesToWatch(){
+    return(
+        <div>
+            <h1> Movies to watch </h1>
+            <p>movies from API wil display here</p>
+        </div>
+    )
+}
+
+export default MoviesToWatch;
