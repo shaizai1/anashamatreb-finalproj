bc506894d59da075a38ffc2b13dc39a5a91cdb4c
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-03-16 14:31:20 +0000

    added basics of section 2

---
+++ src/components/section2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CurrentlyWatching(){
+    return(
+        <div>
+            <h1> Currently Watching </h1>
+            <p>movie will render here</p>
+        </div>
+    )
+}
+
+export default CurrentlyWatching;
