647afaf193799163f42781e732ed7e08b08a2cda
Matthew <mbrooks9622@gmail.com>
2023-03-16 16:40:18 +0000

    rendering api

--- MovieSearch/src/components/SearchBar.jsx
+++ MovieSearch/src/components/SearchBar.jsx
@@ -5,29 +5,53 @@ export const SearchBar = () => {
     const [input, setInput] = useState("");
 
     const fetchData = (value) => {
-    
-    fetch("https://www.omdbapi.com/?t=god+father&y=&plot=short&apikey=b5609ae1")
-    .then(response => response.json())
-    .then(json => {
+        // fetch("https://www.omdbapi.com/?t=god+father&y=&plot=short&apikey=b5609ae1")
+        
+        
+        fetch(`https://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=b5609ae1`)
+        .then(resp =>resp.json())
+        .then(data =>console.log(data));
+        
+    }
 
-        console.log(json)
-    });
-  }
+    // fetch ("http://www.omdbapi.com/?apikey=b5609ae1")
+
+// need a results filter
+// const results = json.filter((data) => {
+//     return ()
+//         value data
+//     console.log(results);
+// }
+    // )
+
+//.then notation here:
+//     .then(response => response.json())
+//     .then(json => {
+
+//         console.log(json)
+//     });
+//   }
 
   const handleChange = (value) => {
     setInput(value)
-    fetchData(value)
+    
     }
-
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        fetchData(input)
+        
+        }
 
     return <div className="input-wrapper">
-
+        <form onSubmit ={handleSubmit}>
+        
         <input
             placeholder="Type to search..."
-
+            
             value={input}
             onChange={(e) => handleChange(e.target.value)}
-        />
+            />
+            </form>
     </div>;
 
 }
