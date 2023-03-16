4e43443d24fb862980b020814ebc42503a5660f4
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-03-16 16:40:56 +0000

    final commit of today

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import CurrentlyWatching from './components/section2';
+import FinishedWatching from './components/section3';
+import MoviesToWatch from './components/section1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
+        
         <CurrentlyWatching />
-      </header>
+        
+      
     </div>
   );
 }
