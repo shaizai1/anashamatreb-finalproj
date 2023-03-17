f36a4ea3afb610dd3b01166bc73a4a821a13504d
Matthew <mbrooks9622@gmail.com>
2023-03-17 14:47:03 +0000

    movie search results rendered on console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 
-
 import './App.css'
 import { SearchBar } from './components/SearchBar';
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,78 +1,69 @@
-import React, { useState } from 'react'
+
+import React, { useState, useEffect } from 'react'
 import "./SearchBar.css";
 
 let history = JSON.parse(localStorage.getItem("history")) || [];
 
 export const SearchBar = () => {
-    const [input, setInput] = useState("");
-
-//drop down menu here:
+    const [input, setInput] = useState([]);
 
 
     //create a state no empty string
 
     const fetchData = (value) => {
-        // fetch("https://www.omdbapi.com/?t=god+father&y=&plot=short&apikey=b5609ae1")
-        
 
-        //store data from fetch send to an element
-    
-        //
-        
-        fetch(`https://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=b5609ae1`)
-        .then(resp =>resp.json())
-        .then(data =>{
-            console.log(data.Poster, data.Title)
-            const newMovie = {
-               poster: data.Poster,
-               title: data.Title
-            }
-            history.push(newMovie);
-            localStorage.setItem("history",JSON.stringify(history))
-        });
-        
+        //Store movie data from fetch and send to an element
+        //How do I get what is rendered in console log to now render on the page?
+
+
+        //-- Fetch Request Section --> Just added useEffect here:
+       
+
+        //data in the drop down will only need the setpart. add it to movie to watch.
+        //state at app.js and pass down to section 1 will need both setstate and set value.
+
+
+
+            fetch(`https://www.omdbapi.com/?t=${value}&y=&plot=short&apikey=b5609ae1`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data.Poster, data.Title, data.Year)
+                const newMovie = {
+                    poster: data.Poster,
+                    title: data.Title,
+                    year: data.Year
+                }
+                history.push(newMovie);
+                localStorage.setItem("history", JSON.stringify(history))
+            });
+  
+            
+            //-- Fetch Request Section -->
+            
     }
 
-    // fetch ("http://www.omdbapi.com/?apikey=b5609ae1")
+    const handleChange = (value) => {
+        setInput(value)
 
-// need a results filter
-// const results = json.filter((data) => {
-//     return ()
-//         value data
-//     console.log(results);
-// }
-    // )
-
-//.then notation here:
-//     .then(response => response.json())
-//     .then(json => {
-
-//         console.log(json)
-//     });
-//   }
-
-  const handleChange = (value) => {
-    setInput(value)
-    
     }
     const handleSubmit = (event) => {
         event.preventDefault()
         fetchData(input)
-        
-        }
+
+    }
 
 
-        
+
     return <div className="input-wrapper">
-    <form onSubmit ={handleSubmit}>
-    
-    <input
-        placeholder="Type to search..."
-        
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-        />
+        <form onSubmit={handleSubmit}>
+
+            <input
+                placeholder="Type to search..."
+
+                value={input}
+                onChange={(e) => handleChange(e.target.value)}
+            />
         </form>
-</div>;
+    </div>;
 
 }
