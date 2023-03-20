49d352ddece7e373c02e7ae58e6805d602d3ee8d
analupucci <analupucci@gmail.com>
2023-03-20 21:20:08 +0000

    commiting to change branches

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CurrentlyWatching from './components/section2';
 import FinishedWatching from './components/section3';
 import MoviesToWatch from './components/section1';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/section3.jsx
+++ src/components/section3.jsx
@@ -3,6 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
+
 function FinishedWatching () {
     return(
         <div>
